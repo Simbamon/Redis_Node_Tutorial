ad1efb4565691cf1a63902a89ce4e14743aaf558
Simbamon <sbhan9511@gmail.com>
2022-01-02 13:22:53 +0900

    object redis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fetch = require('node-fetch')
 const redis = require('redis')
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 5000
 const REDIS_PORT = process.env.PORT || 6379
@@ -8,6 +9,7 @@ const REDIS_PORT = process.env.PORT || 6379
 const RedisClient = redis.createClient(REDIS_PORT)
 
 const app = express()
+
 
 function setResponse(username, repos) {
     return `<h2>${username} has ${repos} Github Repos</h2>`
@@ -33,13 +35,14 @@ async function getReposNumber(req, res, next) {
 }
 
 function cache(req, res, next) {
-    console.log("Cache hit")
+    
     const { username } = req.params
     
     RedisClient.get(username, (err, data) => {
         if(err) throw err;
 
         if(data !== null){
+            console.log("Cache hit")
             res.send(setResponse(username, data))
         } 
         else {
@@ -48,6 +51,25 @@ function cache(req, res, next) {
     })
 }
 app.get('/repos/:username', cache, getReposNumber)
+
+app.get('/photos', async(req, res) => {
+    const albumID = req.query.albumID
+    RedisClient.get("photos", async(error, photos) => {
+        if(error) throw error
+        if(photos !== null) {
+            console.log("Cache hit")
+            return res.json(JSON.parse(photos))
+        }
+        else {
+            console.log("Cache miss")
+            const response = await fetch("https://jsonplaceholder.typicode.com/photos")
+            const data = await response.json()
+            RedisClient.setex("photos", 3600, JSON.stringify({data}))
+            res.json(data)
+        }
+        
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
